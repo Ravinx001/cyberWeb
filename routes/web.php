147603175d35f4bf5fae1711aca90f1fa2cb435f
Ravindu Amarasekara <rav.business.lak@gmail.com>
2024-09-27 02:08:25 +0530

    admin dashbord updated user create route removed

--- routes/web.php
+++ routes/web.php
@@ -43,10 +43,6 @@ Route::middleware(['auth', 'admin'])->group(function () {
         return view('admin.dashboard');
     })->name('admin.dashboard');
 
-    Route::get('/admin/register', [RegisteredUserController::class, 'create'])
-        ->name('admin.register.create');
-    Route::post('/admin/register', [RegisteredUserController::class, 'store'])->name('admin.register.store');
-
     Route::get('/admin/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
     Route::patch('/admin/profile', [ProfileController::class, 'update'])->name('admin.profile.update');
     Route::delete('/admin/profile', [ProfileController::class, 'destroy'])->name('admin.profile.destroy');
